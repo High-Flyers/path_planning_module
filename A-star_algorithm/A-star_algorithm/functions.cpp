e8196aba9bea375cbf827d393c132239353d39fe
karlikp <pitera.karol@gmail.com>
2023-11-24 16:23:38 +0100

    first steps with algorithm A-star

---
+++ A-star_algorithm/A-star_algorithm/functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+#include "functions.h"
+
+using namespace std;
+
+list<pair<int, int>> A_star_2D(){
+	vector<pair<int, int>> open, closed;
+	bool serching = true;
+	do
+	{
+
+	} while (serching);
+
+	list < pair<int, int >> path;
+	return path;
+}
---
+++ A-star_algorithm/A-star_algorithm/functions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std; 
+
+list<pair<int, int>> A_star_2D();
